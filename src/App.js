938e8f12b9ea5f072286a164e3726627133c1d20
Jlorden <Jamie.lorden@gmail.com>
2020-05-13 15:24:46 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 function App() {
+  const  [pokemon, setPokemon] = useState([]);
+  const [show, setShowPokemon] = useState(false);
+  
+  useEffect(() => {
+      fetch("https://pokeapi.co/api/v2/pokemon/?limit=150")
+        .then(res => res.json())
+        .then(res => setPokemon(res.results))
+    }, []);
+  
+    const showPoke = (e) => {
+      show === false ? setShowPokemon(true) : setShowPokemon(false);
+    }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <button 
+      className="m-2 btn btn-outline-info"
+      onClick={(e) => showPoke(e)}
+      >
+        Grab a pokemon
+      </button>
+      <div>
+        {
+          show === true ?
+            pokemon.length > 0 && pokemon.map((poke, index, ability, id) => {
+              return (<div className="m2" key={index}>{poke.name}</div>)
+            })
+            :
+            <div className="m-4">
+              <p className="m-2"></p>
+            </div>
+        }
+        </div>
+      </div>
   );
 }
 
